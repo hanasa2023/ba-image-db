7d908dc59f8fa7ce6fe9f82a50bf46bb839253ee
hanasaki <hanasakayui2022@gmail.com>
2024-11-16 00:47:58 +0800

    🐛 Fix some bugs

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { env } from './config'
 import { logger } from './utils/logger'
 import { RedisDatabase } from './db/database'
 import { fetchWithRetry } from './utils/fetch-with-retry'
-import { BASE_URL } from './utils/constants'
-import { getCurrentPage, saveCurrentPage } from './utils/failed-handle'
+import { BASE_URL, PAGE_FILE } from './utils/constants'
+import { saveCurrentPage, getCurrentPage } from './utils/failed-handle'
+import fs from 'fs'
 
 async function index() {
   const getTotal = await fetchWithRetry(`${BASE_URL}/search`, {
@@ -21,42 +22,46 @@ async function index() {
     const json = await getTotal.json()
     total = json.total
   }
-  if (total === 0) process.exit(-1)
   const lastPage = Math.ceil(total / 60)
   logger.info(`Total: ${total}, Last Page: ${lastPage}`)
 
   const url = env.REDIS_URL
-  // logger.info(`Redis URL: ${url}`)
   const redis = new RedisDatabase(url)
+  await redis.connect()
   await redis.setTotal(total)
 
   let startPage = await getCurrentPage()
-  logger.info(`Start Page: ${startPage}`)
-  for (let i = startPage - 1; i < lastPage; i++) {
-    // if (i < 10) continue
+
+  for (let i = startPage; i < lastPage; i++) {
     logger.info(`Page: ${i + 1}`)
-    const response = await fetchWithRetry(`${BASE_URL}/search`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        word: 'ブルーアーカイブ ',
-        blt: 1000,
-        p: i + 1,
-      }),
-    })
-    if (response.ok) {
-      const json = await response.json()
-      await redis.setData(json)
-      await saveCurrentPage(i + 1)
+    try {
+      const response = await fetchWithRetry(`${BASE_URL}/search`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          word: 'ブルーアーカイブ ',
+          blt: 1000,
+          p: i + 1,
+        }),
+      })
+      if (response.ok) {
+        const json = await response.json()
+        await redis.setData(json)
+        await saveCurrentPage(i + 1)
+      }
+    } catch (error) {
+      logger.error(`Error processing page ${i + 1}: ${error}`)
+      await saveCurrentPage(i + 1) // Save the current page before exiting
+      process.exit(1) // Exit with a non-zero status code on error
     }
   }
+  await redis.disconnect()
+  fs.unlinkSync(PAGE_FILE) // 删除文件表示任务完成
 }
 
-index()
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
-  .finally(() => process.exit(0))
+index().catch((error) => {
+  console.error(error)
+  process.exit(1) // Exit with a non-zero status code on error
+})
